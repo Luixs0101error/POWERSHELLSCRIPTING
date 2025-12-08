2ece78af1d7c61098c99f0a7c13d23358d7411a9
Luixs0101error <luquisnancelaza@uide.edu.ec>
2025-12-08 14:50:16 +0000

    cambios

--- script1.ps1
+++ script1.ps1
@@ -6,12 +6,17 @@ function Start-ProgressBar { #Explicación: Declara una función llamada Start-P
         $Title, #Explicación: Parámetro texto para el título que se mostrará en la barra de progreso (actividad).
         
         [Parameter(Mandatory = $true)] #Explicación: Línea en blanco para legibilidad.
-        [int]$Timer
-    )
+        [int]$Timer #Explicación: Parámetro entero que define el tiempo total en segundos que durará la barra de progreso.
+
+
+    )#Explicación: Cierra el bloque param.
     
-    for ($i = 1; $i -le $Timer; $i++) {
-        Start-Sleep -Seconds 1
-        $percentComplete = ($i / $Timer) * 100
+    for ($i = 1; $i -le $Timer; $i++) { #Explicación: Bucle que recorre cada segundo desde 1 hasta $Timer. Controla el avance del progreso.
+
+        Start-Sleep -Seconds 1#Explicación: Detiene la ejecución durante 1 segundo en cada iteración del bucle.
+        $percentComplete = ($i / $Timer) * 100 #Explicación: Calcula el porcentaje completado en base al tiempo transcurrido ($i) y el total ($Timer).
+
+
         Write-Progress -Activity $Title -Status "$i seconds elapsed" -PercentComplete $percentComplete
     }
 } 
